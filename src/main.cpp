788db725b30b29a112324747bb6556d74e7c4151
Vladimir Kiselev <ski3.mdn@yandex.ru>
2017-08-13 16:06:02 +0300

    Add crow, simple web-server example

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <cstdio>
 
+#include <crow.h>
+
+inline void setup_routing(crow::SimpleApp &app) {
+    CROW_ROUTE(app, "/")([]{
+        return "Hello, world!";
+    });
+}
+
+
 int main() {
-    return 0;
+    crow::SimpleApp app;
+    setup_routing(app);
+    app.port(8080).run();
 }
