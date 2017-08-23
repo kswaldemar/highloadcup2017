a7b5508777abdabf6400fa66df9e9c3cc3544a95
Vladimir Kiselev <ski3.mdn@yandex.ru>
2017-08-24 00:19:01 +0300

    More compile options

--- scripts/run-local-full.sh
+++ scripts/run-local-full.sh
@@ -4,5 +4,6 @@ root=~/Development/Projects/highloadcup2017
 mkdir -p /tmp/hlcup-full-run
 pushd /tmp/hlcup-full-run
 ln -s ${root}/testing/hlcupdocs/data/FULL/data ./data
-${root}/build-debug/web-server
+#perf record -g 
+${root}/build-release/web-server
 popd
--- scripts/run-local-train.sh
+++ scripts/run-local-train.sh
@@ -4,6 +4,7 @@ root=~/Development/Projects/highloadcup2017
 mkdir -p /tmp/hlcup-train-run
 pushd /tmp/hlcup-train-run
 ln -s ${root}/testing/hlcupdocs/data/TRAIN/data ./data
-perf record -g ${root}/build-debug/web-server
+#perf record -g 
+${root}/build-debug/web-server
 popd
 rm -rf /tmp/hlcup-train-run
